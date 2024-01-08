993cbf68db15deef698cb51533a53efde9074b9c
Zelie-C <zelie.chemin@gmail.com>
2024-01-08 10:04:20 +0100

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ let app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const port = parseInt(process.env.PORT as string);
+const port = parseInt(process.env.PORT as string) || 4000;
+
+app.get('/',(_, res) => {
+  res.status(200);
+  res.send("Hello World!!");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
